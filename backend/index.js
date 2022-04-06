5f1caa6cd84d1b9ac9c651ce3aadc292f2bbd250
raulescobar-g <raulescobar_g@tamu.edu>
2022-04-06 17:25:57 -0500

    postgress npm lib

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ const utils = require('./utils')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: '',
+  host: '',
+  database: '',
+  password: '',
+  port: 5432,
+})
 
 if (process.env.NODE_ENV === 'development') { require('dotenv').config(); }
 const port = process.env.NODE_ENV === "development" ? 5000 : process.env.PORT; // heroku sets a PORT to env so dont change this or ask raul
