227a4d95c77d3f79c87526a5ce8d9817f2fa5fb6
david-hung <david_hung@tamu.edu>
2022-04-16 11:46:33 -0500

    landing page and products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,37 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Landing } from './views/Landing'
+// import { Landing } from './views/Landing'
 import { Store } from './views/Store'
 import { Login } from './views/Login'
 import { Cart } from './views/Cart'
 import { Payment } from './views/Payment'
 import { History } from './views/History'
 import { Account } from './views/Account'
-import { Navbar } from './components/Navbar'
+// import { Navbar } from './components/Navbar'
+import Template from "./components/template/Template";
+import Landing from "./components/landing/Landing";
+import ProductDetail from "./components/products/detail/ProductDetail";
+import ProductList from "./components/products/ProductList";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
+      {/* <Navbar /> */}
+      <Template>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:slug" element={<ProductDetail />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </Template>
     </div>
   );
 }
