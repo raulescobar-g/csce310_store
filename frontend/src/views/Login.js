a67878fb7b5e1483278ecabde907ac166c866f98
zeeshanvirani <zvirani99@gmail.com>
2022-04-18 17:20:40 -0500

    login frontend done

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -12,11 +12,14 @@ export function Login() {
         event.preventDefault();
         alert("Trying");
         fetch("http://localhost:5000/users/trylogin", {
-            method: 'GET',
+            method: 'POST',
             mode: "no-cors",
             cache: "no-cache", 
             credentials: "same-origin", 
             headers: {"Content-Type": "application/json"}
+        })
+        .then((data) => {
+            
         })
         .catch(res=>{
             console.log("Exception : ",res);
@@ -24,13 +27,14 @@ export function Login() {
     }
 
     return (
-        <div className="login">
+        <div className="login" style={{marginTop:'56px', marginBottom:'10px'}}>
+            <p style={{paddingTop:'10px'}}>Login to your account.</p>
             <form onSubmit={handleSubmit}>
                 <label for="email" class="login-label">Email</label>
-                <input type="email" class="login-text" autoFocus value={email} onChange={(e) => setEmail(e.target.value)}></input><br></br>
+                <input name="username" type="email" class="login-text" autoFocus value={email} onChange={(e) => setEmail(e.target.value)}></input><br></br>
 
                 <label for="password" class="login-label">Password</label>
-                <input type="password" class="login-text" autoFocus value={password} onChange={(e) => setPassword(e.target.value)}></input><br></br>
+                <input name="password" type="password" class="login-text" autoFocus value={password} onChange={(e) => setPassword(e.target.value)}></input><br></br>
 
                 <button block size="lg" type="submit" disabled={!validateForm()}>
                 Login
