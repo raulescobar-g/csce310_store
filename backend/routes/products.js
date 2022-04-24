71b3113c166e5b53b5875f9e9c3bfe032770b013
zeeshanvirani <zvirani99@gmail.com>
2022-04-24 15:57:57 -0500

    products feature integrated

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -5,8 +5,44 @@ router.get('/getproducts', async(req, res) => {
     try {
         console.log("Attempting to get products")
 
-        const products = await req.app.get('pool').query("SELECT * FROM products")
-        console.log( products.rows );
+        const products = await req.app.get('pool').query("SELECT * FROM product")
+        console.log( products.rows )
+
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.post('/updateproduct', async(req, res) => {
+    try {
+        console.log("Attempting to update product")
+
+        const name = req.body.name;
+        const price = req.body.price;
+        const desc = req.body.desc;
+        const brand = req.body.brand;
+
+        const noNameProducts = await req.app.get('pool').query("SELECT * FROM product WHERE product_description=$1 AND product_price=$2 AND product_brand=$3", [desc, price, brand])
+        const noPriceProducts = await req.app.get('pool').query("SELECT * FROM product WHERE product_name=$1 AND product_description=$2 AND product_brand=$3", [name, desc, brand])
+        const noDescProducts = await req.app.get('pool').query("SELECT * FROM product WHERE product_name=$1 AND product_price=$2 AND product_brand=$3", [name, price, brand])
+        const noBrandProducts = await req.app.get('pool').query("SELECT * FROM product WHERE product_name=$1 AND product_price=$2 AND product_description=$3", [name, price, desc])
+
+        if ( noNameProducts.rows.length > 0 ) {
+            console.log("Updating name of product.")
+            const updateProduct = await req.app.get('pool').query("UPDATE product SET product_name=$4 WHERE product_price=$1 AND product_description=$2 AND product_brand=$3", [price, desc, brand, name])
+        } else if ( noPriceProducts.rows.length > 0 ) {
+            console.log("Updating price of product.")
+            const updateProduct = await req.app.get('pool').query("UPDATE product SET product_price=$4 WHERE product_name=$1 AND product_description=$2 AND product_brand=$3", [name, desc, brand, price])
+        } else if ( noDescProducts.rows.length > 0 ) {
+            console.log("Updating description of product.")
+            const updateProduct = await req.app.get('pool').query("UPDATE product SET product_description=$4 WHERE product_name=$1 AND product_price=$2 AND product_brand=$3", [name, price, brand, desc])
+        } else if ( noBrandProducts.rows.length > 0 ) {
+            console.log("Updating brand of product.")
+            const updateProduct = await req.app.get('pool').query("UPDATE product SET product_brand=$4 WHERE product_name=$1 AND product_description=$2 AND product_price=$3", [name, desc, price, brand])
+        } else {
+            console.log("No products available to update");
+        }
+
     } catch (e) {
         console.log(e)
     }
@@ -16,8 +52,31 @@ router.post('/addproduct', async(req, res) => {
     try {
         console.log("Attempting to add a new product.")
 
-        const newProduct = await req.app.get('pool').query("INSERT INTO products () VALUES () RETURNING *")
-        console.log( newProduct.rows )
+        const name = req.body.name;
+        const price = req.body.price;
+        const desc = req.body.desc;
+        const brand = req.body.brand;
+
+        // check if product exists, if it does, update it
+        const allProducts = await req.app.get('pool').query("SELECT * FROM product WHERE product_name=$1 AND product_price=$2 AND product_brand=$3", [name, price, brand])
+        if ( allProducts.rows.length == 0 ) {
+            console.log("Product does not exist. Creating.")
+            const newProduct = await req.app.get('pool').query("INSERT INTO product (product_name, product_description, product_price, product_brand) VALUES ($1, $2, $3, $4) RETURNING *", [name, desc, price, brand])
+            console.log( newProduct.rows )
+        }
+
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.post('/deleteproduct', async(req, res) => {
+    try {
+        console.log("Attempting to delete product")
+
+        const id = req.body.id;
+
+        const products = await req.app.get('pool').query("DELETE FROM product WHERE product_id=$1", [id])
     } catch (e) {
         console.log(e)
     }
--- frontend/src/views/InventoryManager.js
+++ frontend/src/views/InventoryManager.js
@@ -5,6 +5,7 @@ export function InventoryManager() {
     const [price, setPrice] = useState("");
     const [brand, setBrand] = useState("");
     const [description, setDescription] = useState("");
+    const [productID, setID] = useState("");
 
     function handleAddProduct(event) {
         event.preventDefault();
@@ -12,9 +13,9 @@ export function InventoryManager() {
         const data = {  name: document.getElementById("name").value,
                         price:  document.getElementById("price").value,
                         brand: document.getElementById("brand").value,
-                        description: document.getElementById("desc").value }
+                        desc: document.getElementById("desc").value }
 
-        fetch("http://localhost:5000/addproduct", {
+        fetch("http://localhost:5000/products/addproduct", {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(data)
@@ -27,25 +28,75 @@ export function InventoryManager() {
         })
     }
 
+    function handleUpdateProducts(event) {
+        event.preventDefault();
+        
+        const data = {  name: document.getElementById("name").value,
+                        price:  document.getElementById("price").value,
+                        brand: document.getElementById("brand").value,
+                        desc: document.getElementById("desc").value }
+
+        fetch("http://localhost:5000/products/updateproduct", {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        })
+        .then((data) => {
+            
+        })
+        .catch(res=>{
+            console.log("Exception : ",res);
+        })
+    }
+
+    function handleGetProducts(event) {
+        event.preventDefault();
+
+        fetch("http://localhost:5000/products/getproducts")
+        .then((res) => res.json())
+
+    }
+    
+    function handleDeleteProduct(event) {
+        event.preventDefault();
+        const data = {
+            id: document.getElementById("productid").value
+        }
+        fetch("http://localhost:5000/products/deleteproduct", {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        })
+    }
+
     return (
         <div className="productManagement" style={{marginTop:'56px', marginBottom:'10px'}}>
-            <p style={{paddingTop:'10px'}}>Add a new product</p>
+            <button size="lg" type="button" style={{marginTop:'15px'}} onClick={handleGetProducts}>Get Products</button><br></br>
+            <br></br>           
+            <p style={{paddingTop:'5px'}}>Add a new product or Update an existing product</p>
             <form onSubmit={handleAddProduct}>
                 <label for="name" class="login-label">Product Name</label>
                 <input name="name" id="name" type="text" class="login-text" autoFocus value={name} onChange={(e) => setName(e.target.value)}></input><br></br>
 
-                <label for="price" class="login-label">Password</label>
+                <label for="price" class="login-label">Product Price</label>
                 <input name="price" id="price" type="text" class="login-text" autoFocus value={price} onChange={(e) => setPrice(e.target.value)}></input><br></br>
 
-                <label for="brand" class="login-label">Password</label>
+                <label for="brand" class="login-label">Product Brand</label>
                 <input name="brand" id="brand" type="text" class="login-text" autoFocus value={brand} onChange={(e) => setBrand(e.target.value)}></input><br></br>
 
-                <label for="desc" class="login-label">Password</label>
+                <label for="desc" class="login-label">Product Description</label>
                 <input name="desc" id="desc" type="text" class="login-text" autoFocus value={description} onChange={(e) => setDescription(e.target.value)}></input><br></br>
 
-                <button block size="lg" type="submit">
-                Add
-                </button>
+                <button block size="lg" type="submit">Add</button>
+                <button block size="lg" style={ {marginLeft:'10px'} } onClick={handleUpdateProducts}>Update</button>
+            </form><br></br>
+
+            
+
+            <form onSubmit={handleDeleteProduct}>
+                <label for="productid" class="login-label">Product ID to Delete</label>
+                <input name="productid" id="productid" type="text" class="login-text" autoFocus value={productID} onChange={(e) => setID(e.target.value)}></input>
+                <button block size="lg" type="submit" style={ {marginLeft:'10px'} }>Delete</button>
             </form>
         </div>
     )
