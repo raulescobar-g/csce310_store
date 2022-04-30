7a6ac4560aeeed0ceb4e6895f64ee995c52a0022
david-hung <david_hung@tamu.edu>
2022-04-29 23:34:25 -0500

    linked new arrivals to product/:id page

--- frontend/src/components/landing/FeatureProduct.js
+++ frontend/src/components/landing/FeatureProduct.js
@@ -15,7 +15,7 @@ function FeatureProduct(props) {
           <h5 className="card-title text-center">{props.product_name}</h5>
           <p className="card-text text-center text-muted">${props.product_price}</p>
           <div className="d-grid gap-2">
-            <Link to="/products/1" className="btn btn-outline-dark" replace>
+            <Link to={`/products/${props.product_id}`} state={props} className="btn btn-outline-dark" replace>
               Detail
             </Link>
           </div>
